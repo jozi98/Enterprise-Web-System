0e31363312549e34870e04b0371f68801d01f6e9
jozi98 <35927030+jozi98@users.noreply.github.com>
2020-02-12 11:24:24 +0000

    added the controller

---
+++ controller.php
@@ -0,0 +1,14 @@
+class controller
+{
+  private $model;
+  
+  public function __construct($model){
+    $this->model = $model;
+    
+   }
+   
+   public function clicked(){
+      
+      $this->model->string = "using the controller to update the view;
+      }
+   }
