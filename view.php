32dc8a3a086679a477247fb3573e1cb7e12e0512
jozi98 <35927030+jozi98@users.noreply.github.com>
2020-02-12 11:27:07 +0000

    added the view.php

---
+++ view.php
@@ -0,0 +1,16 @@
+class View
+{
+  private $model;
+  private $controller;
+  
+  public function __construct($model,$constructor){
+    $this->model = $model;
+    $this->controller= $controller;
+    
+   }
+   
+   public function output(){
+      
+     return '<p><a href = "mvc.php?action=clicked">'.$this->model->string."</a><p>";
+      }
+   }
